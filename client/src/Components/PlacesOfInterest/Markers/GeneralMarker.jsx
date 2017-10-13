fb58bca093e3f38ade2f74e0d58e4534b6976141
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-13 14:36:23 -0700

    Add hover style to map markers

--- client/src/Components/PlacesOfInterest/Markers/GeneralMarker.jsx
+++ client/src/Components/PlacesOfInterest/Markers/GeneralMarker.jsx
@@ -7,8 +7,8 @@ class GeneralMarker extends React.Component {
   render() {
     const GeneralMarkerStyle = {
       position: 'absolute',
-      width: 25,
-      height: 25,
+      width: M_CIRCLE_WIDTH,
+      height: M_CIRCLE_HEIGHT,
       left: -10,
       top: -10,
 
@@ -24,15 +24,39 @@ class GeneralMarker extends React.Component {
     };
 
     const GeneralMarkerHoverStyle = {
+      position: 'absolute',
+      width: M_CIRCLE_WIDTH,
+      height: M_CIRCLE_HEIGHT,
+      left: -10,
+      top: -10,
 
+      backgroundColor: 'red',
+      textAlign: 'center',
+      fontSize: 16,
+      color: '#fff',
+      paddingTop: 2,
+      borderRadius: '50%',
+      border: '1px solid white',
+      boxShadow: '0 0.5px 0.5px rgba(0, 0, 0, 0.8), 0 0 0 9px rgba(255, 255, 255, 0.7)',
+      zIndex: -1
     };
 
+    const MarkerStyle = this.props.$hover ? GeneralMarkerHoverStyle : GeneralMarkerStyle;
+
     return (
-      <div className="map-marker" style={GeneralMarkerStyle}>
+      <div className="map-marker" style={MarkerStyle}>
         <i className="fa fa-map-marker"></i>
       </div>
     )
   }
 }
+
+GeneralMarker.PropTypes = {
+  $hover: PropTypes.bool,
+  text: PropTypes.string,
+  zIndex: PropTypes.number
+};
+
+GeneralMarker.defaultProps = {};
 
 export default GeneralMarker;
