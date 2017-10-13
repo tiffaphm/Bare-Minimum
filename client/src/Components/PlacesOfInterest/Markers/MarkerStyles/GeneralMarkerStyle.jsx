1796dac9ffef97a04e01a00a1360c35e242402be
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-12 23:08:06 -0700

    create table in database

---
+++ client/src/Components/PlacesOfInterest/Markers/MarkerStyles/GeneralMarkerStyle.jsx
@@ -0,0 +1,13 @@
+const GeneralMarkerStyle = {
+  position: 'absolute',
+  width: 20,
+  height: 20,
+  left: -10,
+  top: -10,
+  marginLeft: -1,
+  border: '1px solid white',
+
+  backgroundColor: 'red'
+}
+
+export default GeneralMarkerStyle;
--- client/src/Components/PlacesOfInterest/TripMap.jsx
+++ client/src/Components/PlacesOfInterest/TripMap.jsx
@@ -2,124 +2,83 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import GoogleApiKey from "./GoogleApiKey.jsx";
-import scriptLoader from 'react-async-script-loader';
+import GoogleMap from "google-map-react";
 
-// import GeneralMarker from "./Markers/GeneralMarker.jsx";
-import { GeneralMarkerStyle, GeneralMarkerHoverStyle } from "./Markers/GeneralMarker.jsx";
-import InputBoxForMap from './InputBoxForMap.jsx';
+import GeneralMarker from "./Markers/GeneralMarker.jsx";
 
+// const loadJS = (src) => {
+//     var ref = window.document.getElementsByTagName("script")[0];
+//     var script = window.document.createElement("script");
+//     script.src = src;
+//     script.async = true;
+//     ref.parentNode.insertBefore(script, ref);
+// }
 
 class TripMap extends React.Component {
   constructor(props) {
     super(props);
     this.addMarker = this.addMarker.bind(this);
-    // this.markerHover = this.markerHover.bind(this);
-    this.toggleInfoBox = this.toggleInfoBox.bind(this);
 
     this.state = {
-      markers: [this.props.tripCoords],
-      newMarker: false
+      markers: [this.props.tripCoords]
     }
   }
 
-  componentWillReceiveProps({ isScriptLoadSucceed }) {
-    let self = this;
-    if (isScriptLoadSucceed) {
-      this.map = new google.maps.Map(this.refs.map, {
-        center: this.props.tripCoords,
-        zoom: this.props.zoom
-      });
-
-      let searchbox = this.refs.searchbox;
-      let searchBox = new google.maps.places.SearchBox(searchbox);
-      this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchboxdiv);
-      console.log(this.state.searchbox);
-
-      // this.map.addListener('bounds_changed', function() {
-      //   searchBox.setBounds(self.map.getBounds());
-      // })
-
-      // let markers = [];
-
-      // searchBox.addListener('places_changed', function() {
-      //   let places = searchBox.getPlaces();
-
-      //   if (places.length === 0) {
-      //     return;
-      //   }
-
-      //   markers.forEach(marker => {
-      //     marker.setMap(null);
-      //   });
-      //   markers = [];
-
-      //   this.bounds = new google.maps.LatLngBounds();
-      //   places.forEach(function(place) {
-      //     if (!place.geometry) {
-      //       console.log('returned place contains no geometry');
-      //       return;
-      //     }
-
-      //     markers.push(new google.maps.Marker({
-      //       map: this.map,
-      //       title: place.name,
-      //       position: place.geometry.location
-      //     }));
-
-      //     if (place.geometry.viewport) {
-      //       bounds.union(place.geometry.viewport);
-      //     } else {
-      //       bounds.extend(place.geometry.location);
-      //     }
-      //   });
-      //   map.fitBounds(bounds);
-      // });
-      
-
-    } // end of if statement
-  } // end of componentwillreceiveprops
-
-  markerHover(marker) {
-    console.log('hi');
+  getLocationForTrip() {
+    // if (navigator.geolocation) {
+    //   navigator.geolocation.getCurrentPosition(position => {
+    //     let pos = {
+    //       lat: position.coords.latitude,
+    //       lng: position.coords.lo
+    //     }
+    //   })
+    // }
   }
 
-  addMarker(event) {
-    console.log(window.google);
-  }
+  addMarker(obj) {
+    let lat = obj.lat;
+    let lng = obj.lng;
+    let coords = {
+      'lat': lat,
+      'lng': lng
+    }
 
-  toggleInfoBox() {
+    let newCoords = this.state.markers.slice();
+    newCoords.push(coords);
     this.setState({
-      newMarker: !this.state.newMarker
+      markers: newCoords
     })
   }
 
   render() {
-    // let markers = this.state.markers.map((item, index) => (
-    //   <GeneralMarker key={index} lat={item.lat} lng={item.lng}/>
-    // ))
-
+    let markers = this.state.markers.map((item, key) => (
+      <GeneralMarker key={key} lat={item.lat} lng={item.lng} onClick={this.removeMarker}/>
+    ))
     return (
       <div className="trip-map-container">
-        <input id="pac-input" className="controls" type="text" placeholder="Search Box" ref="searchbox" />
-        <div ref="map">
-        </div>
+        <GoogleMap
+          bootstrapURLKeys={{ key: GoogleApiKey }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+          onClick={this.getLatLng}
+        >
+          {markers}
+        </GoogleMap>
       </div>
     );
   }
 }
 
 TripMap.propTypes = {
-  center: PropTypes.any,
-  latLng: PropTypes.array,
+  center: PropTypes.array,
   zoom: PropTypes.number,
   tripCoords: PropTypes.any
 };
 
 TripMap.defaultProps = {
-  center: { lat: 37.783667, lng: -122.408885 },
-  latLng: [37.783667, -122.408885],
+  center: [37.783667, -122.408885],
   zoom: 15,
   tripCoords: { lat: 37.783667, lng: -122.408885 }
 };
 
-export default scriptLoader([GoogleApiKey])(TripMap);
+export default TripMap;
