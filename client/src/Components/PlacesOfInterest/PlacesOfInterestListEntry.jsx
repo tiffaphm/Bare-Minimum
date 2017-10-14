29b95a1085d153104a93a687b0830c55c69d4bdf
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-14 12:51:55 -0700

    Correctly show searched place info on list entry

--- client/src/Components/PlacesOfInterest/PlacesOfInterestListEntry.jsx
+++ client/src/Components/PlacesOfInterest/PlacesOfInterestListEntry.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
 
-class PlacesOfInterestListEntry extends React.Component {
-
-  saveDataForPlaceOfInterest() {
-    //post request here
-    console.log('kweh! saved!')
-  }
-
-  render() {
+const PlacesOfInterestListEntry = (props) => {
     return (
       <div className="list-group list-group-flush small">
-        <a className="list-group-item list-group-item-action" href="#">
+        <span className="list-group-item list-group-item-action" href="#">
           <div className="media">
             <img className="d-flex mr-3 rounded-circle" src="http://placehold.it/45x45" alt=""/>
             <div className="media-body">
-              <strong>Place Name</strong><br/>
-              description
+              <strong>{props.place.name}</strong><br/>
+              <input type="text" value="user notes"/>
               <div className="text-muted smaller">
-                address · phone · web
+                {props.place.formatted_address}<br/>
+                {props.place.formatted_phone_number}<br/>
+                <a href={props.place.website}>{props.place.website}</a>
               </div>
             </div>
           </div>
-        </a>
+        </span>
       </div>
     )
-  }
 };
 
 // PlacesOfInterestListEntry.proptypes = {
