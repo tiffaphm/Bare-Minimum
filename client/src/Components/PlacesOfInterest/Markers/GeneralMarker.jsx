4d3ac58cd8082a60a45505958e9d0ea3d7e2c61d
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-13 19:04:41 -0700

    Refactor to use google maps api

--- client/src/Components/PlacesOfInterest/Markers/GeneralMarker.jsx
+++ client/src/Components/PlacesOfInterest/Markers/GeneralMarker.jsx
@@ -1,70 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+const GeneralMarkerStyle = {
+  position: 'absolute',
+  width: 25,
+  height: 25,
+  left: -10,
+  top: -10,
 
-const M_CIRCLE_WIDTH = 25;
-const M_CIRCLE_HEIGHT = 25;
-
-import InputBoxForMap from '../InputBoxForMap.jsx';
-
-class GeneralMarker extends React.Component {
-  componentDidMount() {
-    let form = React.findDOMNode(this.refs.form);
-    this.inputBox = new google.maps.InfoWindow(form);
-  }
-  
-  render() {
-    const GeneralMarkerStyle = {
-      position: 'absolute',
-      width: M_CIRCLE_WIDTH,
-      height: M_CIRCLE_HEIGHT,
-      left: -10,
-      top: -10,
-
-      backgroundColor: 'red',
-      textAlign: 'center',
-      fontSize: 16,
-      color: '#fff',
-      paddingTop: 2,
-      borderRadius: '50%',
-      border: '1px solid white',
-      boxShadow: '0 0.5px 0.5px rgba(0, 0, 0, 0.8)',
-      zIndex: -1
-    };
-
-    const GeneralMarkerHoverStyle = {
-      position: 'absolute',
-      width: M_CIRCLE_WIDTH,
-      height: M_CIRCLE_HEIGHT,
-      left: -10,
-      top: -10,
-
-      backgroundColor: 'red',
-      textAlign: 'center',
-      fontSize: 16,
-      color: '#fff',
-      paddingTop: 2,
-      borderRadius: '50%',
-      border: '1px solid white',
-      boxShadow: '0 0.5px 0.5px rgba(0, 0, 0, 0.8), 0 0 0 9px rgba(255, 255, 255, 0.7)',
-      zIndex: -1
-    };
-
-    const MarkerStyle = this.props.$hover ? GeneralMarkerHoverStyle : GeneralMarkerStyle;
-
-    return (
-      <div className="map-marker" style={MarkerStyle}>
-        <i className="fa fa-map-marker"></i>
-      </div>
-    )
-  }
-}
-
-GeneralMarker.PropTypes = {
-  $hover: PropTypes.bool,
-  text: PropTypes.string,
-  zIndex: PropTypes.number
+  backgroundColor: 'red',
+  textAlign: 'center',
+  fontSize: 16,
+  color: '#fff',
+  paddingTop: 2,
+  borderRadius: '50%',
+  border: '1px solid white',
+  boxShadow: '0 0.5px 0.5px rgba(0, 0, 0, 0.8)',
+  zIndex: -1
 };
 
-GeneralMarker.defaultProps = {};
+const GeneralMarkerHoverStyle = {
+  position: 'absolute',
+  width: 25,
+  height: 25,
+  left: -10,
+  top: -10,
 
-export default GeneralMarker;
+  backgroundColor: 'red',
+  textAlign: 'center',
+  fontSize: 16,
+  color: '#fff',
+  paddingTop: 2,
+  borderRadius: '50%',
+  border: '1px solid white',
+  boxShadow: '0 0.5px 0.5px rgba(0, 0, 0, 0.8), 0 0 0 9px rgba(255, 255, 255, 0.7)',
+  zIndex: -1
+};
+
+export default { GeneralMarkerStyle, GeneralMarkerHoverStyle };
