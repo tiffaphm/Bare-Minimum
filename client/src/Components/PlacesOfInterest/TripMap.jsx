e46a76749636e1e632211cbf8ce69e945b667670
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-13 23:14:58 -0700

    Google maps potentially throw away

--- client/src/Components/PlacesOfInterest/TripMap.jsx
+++ client/src/Components/PlacesOfInterest/TripMap.jsx
@@ -12,7 +12,8 @@ import InputBoxForMap from './InputBoxForMap.jsx';
 class TripMap extends React.Component {
   constructor(props) {
     super(props);
-    // this.addMarker = this.addMarker.bind(this);
+    this.addMarker = this.addMarker.bind(this);
+    // this.markerHover = this.markerHover.bind(this);
     this.toggleInfoBox = this.toggleInfoBox.bind(this);
 
     this.state = {
@@ -26,6 +27,22 @@ class TripMap extends React.Component {
       this.map = new google.maps.Map(this.refs.map, {
         center: this.props.tripCoords,
         zoom: this.props.zoom
+      });
+
+      let infoBoxDiv = `<div className="input-location-information-container">
+          <div id="form">
+            <table><tbody>
+              <tr><td>name:</td><td><input type='text' id='name'/></td></tr>
+              <tr><td>address:</td><td><input type='text' id='address'/></td></tr>
+              <tr><td>type:</td><td><input type='text' id='type'/></td></tr>
+              <tr><td>notes:</td><td><input type='text' id='notes'/></td></tr>
+              <tr><td></td><td><input type='button' value='Save' onClick={this.saveDataForPlaceOfInterest}/></td></tr>
+            </tbody></table>
+          </div>
+        </div>`;
+      this.infowindow = new google.maps.InfoWindow({
+        content: infoBoxDiv
+        // pixelOffset: new google.maps.Size(200,0)
       });
 
       class GeneralMarker extends google.maps.OverlayView {
@@ -62,13 +79,50 @@ class TripMap extends React.Component {
             //   div.markers.marker_id = self.args.marker_id;
             // }
 
+            // google.maps.event.addDomListener(div, 'click', () = {
+            //   infowindow.open(map, div);
+            // });
+
             google.maps.event.addDomListener(div, 'click', () => {
               google.maps.event.trigger(self, 'click');
+            });
+
+            google.maps.event.addDomListener(div, 'mouseover', () => {
+              div.style.position = 'absolute';
+              div.style.cursor = 'pointer';
+              div.style.width = '25px';
+              div.style.height = '25px';
+              div.style.backgroundColor = '#C70039';
+              div.style.textAlign = 'center';
+              div.style.fontSize = '16px';
+              div.style.color = '#fff';
+              div.style.paddingTop = '2px';
+              div.style.borderRadius = '50%';
+              div.style.border = '1px solid white';
+              div.style.boxShadow = '0 0.5px 0.5px rgba(0, 0, 0, 0.8), 0 0 0 9px rgba(255, 255, 255, 0.7)';
+              div.style.zIndex = '-1';
+            })
+
+            google.maps.event.addDomListener(div, 'mouseout', () => {
+              div.style.position = 'absolute';
+              div.style.cursor = 'pointer';
+              div.style.width = '25px';
+              div.style.height = '25px';
+              div.style.backgroundColor = '#C70039';
+              div.style.textAlign = 'center';
+              div.style.fontSize = '16px';
+              div.style.color = '#fff';
+              div.style.paddingTop = '2px';
+              div.style.borderRadius = '50%';
+              div.style.border = '1px solid white';
+              div.style.boxShadow = '0 0.5px 0.5px rgba(0, 0, 0, 0.8)';
+              div.style.zIndex = '-1';
             });
 
             let panes = this.getPanes();
             panes.overlayImage.appendChild(div);
           };
+
           let point = this.getProjection().fromLatLngToDivPixel(this.latlng);
 
           if (point) {
@@ -80,25 +134,24 @@ class TripMap extends React.Component {
 
       google.maps.event.addDomListener(this.map, "click", (event) => {
         let marker = new GeneralMarker(event.latLng, this.map);
+            this.infowindow.setPosition(event.latLng);
+            this.infowindow.open(this.map, marker);
+            // console.log(marker.latlng.lat());
+        // });
       });
+
+
 
     } // end of if statement
   } // end of componentwillreceiveprops
 
-  // addMarker(obj) {
-  //   let lat = obj.lat;
-  //   let lng = obj.lng;
-  //   let coords = {
-  //     'lat': lat,
-  //     'lng': lng
-  //   }
+  markerHover(marker) {
+    console.log('hi');
+  }
 
-  //   let newCoords = this.state.markers.slice();
-  //   newCoords.push(coords);
-  //   this.setState({
-  //     markers: newCoords
-  //   })
-  // }
+  addMarker(event) {
+    console.log(window.google);
+  }
 
   toggleInfoBox() {
     this.setState({
@@ -113,7 +166,7 @@ class TripMap extends React.Component {
 
     return (
       <div className="trip-map-container">
-        <div ref="map">
+        <div ref="map" onClick={this.addMarker}>
         Loading...
         </div>
       </div>
