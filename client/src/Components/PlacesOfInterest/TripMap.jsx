4d18d2adaa3476bb7f426a07168f89ee48681c93
Tiffany Pham <tiffanynpham@gmail.com>
2017-10-14 10:08:14 -0700

    Fix notifications panel css

--- client/src/Components/PlacesOfInterest/TripMap.jsx
+++ client/src/Components/PlacesOfInterest/TripMap.jsx
@@ -2,24 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import GoogleApiKey from "./GoogleApiKey.jsx";
-import scriptLoader from 'react-async-script-loader';
+import scriptLoader from "react-async-script-loader";
 
 // import GeneralMarker from "./Markers/GeneralMarker.jsx";
-import { GeneralMarkerStyle, GeneralMarkerHoverStyle } from "./Markers/GeneralMarker.jsx";
-import InputBoxForMap from './InputBoxForMap.jsx';
+import {
+  GeneralMarkerStyle,
+  GeneralMarkerHoverStyle
+} from "./Markers/GeneralMarker.jsx";
+import InputBoxForMap from "./InputBoxForMap.jsx";
 
+let searchedPlace = {};
 
 class TripMap extends React.Component {
   constructor(props) {
     super(props);
     this.addMarker = this.addMarker.bind(this);
     // this.markerHover = this.markerHover.bind(this);
+    this.getPlaceInfo = this.getPlaceInfo.bind(this);
     this.toggleInfoBox = this.toggleInfoBox.bind(this);
-
     this.state = {
       markers: [this.props.tripCoords],
       newMarker: false
-    }
+    };
   }
 
   componentWillReceiveProps({ isScriptLoadSucceed }) {
@@ -31,56 +35,78 @@ class TripMap extends React.Component {
       });
 
       let searchbox = this.refs.searchbox;
-      let searchBox = new google.maps.places.SearchBox(searchbox);
+      let AutoComplete = new google.maps.places.Autocomplete(searchbox);
+      AutoComplete.bindTo("bounds", this.map);
+
       this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(searchbox);
 
-      this.map.addListener('bounds_changed', () => {
-        searchBox.setBounds(this.map.getBounds());
+      let infowindow = new google.maps.InfoWindow();
+      let infowindowContent = this.refs.infowindow;
+      infowindow.setContent(infowindowContent);
+
+      // let savedInfo = new google.maps.InfoWindow();
+      // let savedInfoContent = this.refs.savedinfowindow;
+      // savedInfo.setContent(savedInfoContent);
+
+      this.marker = new google.maps.Marker({
+        map: this.map
+      });
+
+      this.marker.addListener('click', () => {
+        infowindow.open(this.map, this.marker);
       })
 
-      let markers = [];
+      this.marker.addListener('mouseover', () => {
+        infowindow.open(this.map, this.marker);
+      })
 
-      searchBox.addListener('places_changed', () => {
-        let places = searchBox.getPlaces();
+      this.marker.addListener('mouseout', () => {
+        infowindow.open(this.map, this.marker);
+      })
 
-        if (places.length === 0) {
+      AutoComplete.addListener('place_changed', () => {
+        infowindow.close();
+        let place = AutoComplete.getPlace();
+        if (!place.geometry) {
           return;
         }
-      })
 
-      //   markers.forEach(marker => {
-      //     marker.setMap(null);
-      //   });
-      //   markers = [];
+        if (place.geometry.viewport) {
+          this.map.fitBounds(place.geometry.viewport);
+        } else {
+          this.map.setCenter(place.geometry.location);
+          this.map.setZoom(17);
+        }
 
-      //   this.bounds = new google.maps.LatLngBounds();
-      //   places.forEach(function(place) {
-      //     if (!place.geometry) {
-      //       console.log('returned place contains no geometry');
-      //       return;
-      //     }
+        // Set the position of the marker using the place ID and location
+        this.marker.setPlace({
+          placeId: place.place_id,
+          location: place.geometry.location
+        });
+        this.marker.setVisible(true);
 
-      //     markers.push(new google.maps.Marker({
-      //       map: this.map,
-      //       title: place.name,
-      //       position: place.geometry.location
-      //     }));
-
-      //     if (place.geometry.viewport) {
-      //       bounds.union(place.geometry.viewport);
-      //     } else {
-      //       bounds.extend(place.geometry.location);
-      //     }
-      //   });
-      //   map.fitBounds(bounds);
-      // });
-      
+        infowindowContent.children['place-name'].textContent = place.name;
+        // infowindowContent.children['place-id'].textContent = place.place_id;
+        infowindowContent.children['place-address'].textContent = place.formatted_address;
+        infowindowContent.children['place-phone'].textContent = place.formatted_phone_number;
+        infowindowContent.children['place-website'].textContent = place.website;
+        this.getPlaceInfo(place);
+        infowindow.open(this.map, this.marker);
+      });
 
     } // end of if statement
   } // end of componentwillreceiveprops
 
   markerHover(marker) {
-    console.log('hi');
+    console.log("hi");
+  }
+
+  getPlaceInfo(place) {
+    searchedPlace = place
+  }
+
+  savePlaceInfo() {
+    //post request to db
   }
 
   addMarker(event) {
@@ -90,7 +116,7 @@ class TripMap extends React.Component {
   toggleInfoBox() {
     this.setState({
       newMarker: !this.state.newMarker
-    })
+    });
   }
 
   render() {
@@ -99,9 +125,32 @@ class TripMap extends React.Component {
     // ))
 
     return (
-      <div className="trip-map-container">
-        <input id="pac-input" className="controls" type="text" placeholder="Search Box" ref="searchbox" />
-        <div ref="map">
+      <div className="trip-container">
+        <div className="trip-map-container col-md-10 col-sm-8">
+          <input
+            ref="searchbox"
+            id="pac-input"
+            className="controls"
+            type="text"
+            placeholder="Search..."
+          />
+          <div ref="map" />
+          <div ref="savedinfowindow">
+            <span id="place-name" className="title" />
+          </div>
+          <div ref="infowindow">
+            <span id="place-name" className="title" />
+            <br />
+            <span id="place-address" className="place-address" />
+            <br />
+            <span id="place-phone" className="place-phone" />
+            <br />
+            <span id="place-website" className="place-website" />
+            <br />
+            <input type="button" value='save' onClick={this.savePlaceInfo} className="save-place-button" />
+          </div>
+        </div>
+        <div className="trip-places-list-container">
         </div>
       </div>
     );
